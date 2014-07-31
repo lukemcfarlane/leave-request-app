ddedb8a844b624006ff8cce64a40136a7bb0f539
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-07-31 21:15:05 +1200

    Skeleton Visualforce page and AngularJS app

---
+++ resource-bundles/LeaveRequestApp.resource/app/controllers/mainCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * This is the main controller for the app used for LeaveRequestsApp
+ * Visualforce page.
+ *
+ * @author  https://github.com/lukemcfarlane
+ * @date    July 2014
+ */
+app.controller('mainCtrl', function($scope) {});
