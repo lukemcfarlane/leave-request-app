3964103dfb041a6d4e9ceea57a1a095afff44df7
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-08-01 10:02:58 +1200

    Added init function in controller to load leave requests on load

--- resource-bundles/LeaveRequestApp.resource/app/controllers/mainCtrl.js
+++ resource-bundles/LeaveRequestApp.resource/app/controllers/mainCtrl.js
@@ -5,4 +5,20 @@
  * @author  https://github.com/lukemcfarlane
  * @date    July 2014
  */
-app.controller('mainCtrl', function($scope) {});
+app.controller('mainCtrl', ['$scope', 'LeaveRequestService',
+	function($scope, LeaveRequestService) {
+		$scope.isLoading = false;
+		$scope.allLeaveRequests = [];
+
+		function init() {
+			$scope.isLoading = true;
+			LeaveRequestService.getAllLeaveRequests()
+				.then(function(results) {
+					$scope.isLoading = false;
+					$scope.allLeaveRequests = results;
+				}, function(err) {});
+		}
+
+		init();
+	}
+]);
