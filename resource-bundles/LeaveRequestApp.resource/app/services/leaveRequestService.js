af1eaac64f298abba5d08a8b04fc8f996e0b2617
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-08-01 20:37:49 +1200

    Use sfrquery instead of sfr

--- resource-bundles/LeaveRequestApp.resource/app/services/leaveRequestService.js
+++ resource-bundles/LeaveRequestApp.resource/app/services/leaveRequestService.js
@@ -5,14 +5,14 @@
  * @author  https://github.com/lukemcfarlane
  * @date    July 2014
  */
-app.factory('LeaveRequestService', ['sfr',
-	function(sfr) {
+app.factory('LeaveRequestService', ['sfrquery',
+	function(sfrquery) {
 		var LeaveRequestService = {
 			getAllLeaveRequests: function() {
-				var queryStr = 'SELECT Id, Start_Date__c, End_Date, Reason__c, Approved__c, Employee__r.Name ' +
+				var queryStr = 'SELECT Id, Start_Date__c, End_Date__c, Reason__c, Approved__c, Employee__r.Name ' +
 					'FROM Leave_Request__c';
 				var cacheEnabled = false;
-				return sfr.query(queryStr, cacheEnabled);
+				return sfrquery.query(queryStr, cacheEnabled);
 			}
 		};
 		return LeaveRequestService;
